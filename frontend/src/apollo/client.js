f8d2e6037b52ab382c9de1b257c638ce75192992
alexa <alexandr191261@yandex.ru>
2024-01-06 00:31:33 +0700

    Add API Link for Build

--- frontend/src/apollo/client.js
+++ frontend/src/apollo/client.js
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4444/graphql",
+  
+  uri: import.meta.env.DEV ? "http://localhost:4444/graphql" : "http://185.182.111.231:4444/graphql",
   cache: new InMemoryCache(),
 });
 
