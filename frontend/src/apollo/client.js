c09e8b261e1daa99151cc2c3dfaa8e5cb45dd2e4
alexa <alexandr191261@yandex.ru>
2024-01-06 05:16:13 +0700

    Frontent Build Link FIX

--- frontend/src/apollo/client.js
+++ frontend/src/apollo/client.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
 const client = new ApolloClient({
   
-  uri: import.meta.env.DEV ? "http://localhost:4444/graphql" : "http://185.182.111.231:4444/graphql",
+  uri: import.meta.env.DEV ? "http://localhost:4444/graphql" : "https://vertera-ticket.yasanyabeats.ru:4444/graphql",
   cache: new InMemoryCache(),
 });
 
