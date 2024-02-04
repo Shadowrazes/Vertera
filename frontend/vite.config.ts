4e465c3cd841fca02043fbba28180266c06e9d8c
YaSanyaBeats <alexandr191261@yandex.ru>
2024-02-05 02:05:11 +0700

    Fix External graphql

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,11 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: 'localhost', 
+    hmr: {host: 'localhost'}, 
+},
+
   plugins: [react()],
 
-  build: {
-  rollupOptions: {
-  external: ['graphql'],
-  },
-  },
+  /*build: {
+    rollupOptions: {
+      external: [],
+    },
+  },*/
   });
